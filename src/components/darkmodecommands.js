8563cdffbdcff56ec7c44cc7e20a3e0c0de530b3
Ping Lee <49118143+TristanSamuelAlfre@users.noreply.github.com>
2019-12-30 16:00:30 +0800

    安装APEX

--- src/components/darkmodecommands.js
+++ src/components/darkmodecommands.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Toggle from '.tToggle';
 import useDarkMode from 'use-dark-mode';
 
 const DarkModeCommands = () => {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import "prismjs/themes/prism.css"
+
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
