8b99f85b69e012ed634eb8a98dd457f04f6950b5
Ping Lee <49118143+TristanSamuelAlfre@users.noreply.github.com>
2020-01-09 16:45:19 +0800

    更新

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-//app.get('/bkimg', (req, res) => res.send('Hello World!'))
-
-app.listen(80, () => console.log('App listening on port 80!'))
+app.listen(80, () => console.log('App listening on port 80!'));
